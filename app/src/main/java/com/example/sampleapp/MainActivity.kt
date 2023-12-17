c6e0819071db624c2d6651e38baf03392c2432b9
SreejithThrivikraman <mail.sreejith.23@gmail.com>
2023-12-17 15:29:24 -0500

    fixed the triggering issue of compose function with the 'mutableListOf'. replaced 'mutableListOf' with 'mutableStateListOf' on line 23.

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -9,16 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 
-val nameList = mutableListOf("Jacob", "Raswan", "Zaheer")
+val nameList = mutableStateListOf("Jacob", "Raswan", "Zaheer")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,27 +32,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    val greetingState = remember { mutableStateListOf("Jacob", "Raswan", "Zaheer")}
+    val greetingState by remember { mutableStateOf(nameList) }
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
 
-        for (item in greetingState) {
-            Text(
-                text = "$item",
-            )
+        greetingState.forEach { item ->
+            Text(text = item)
         }
 
-        Button(
-            onClick = { greetingState.add("1")
-                         greetingState = greetingState.toMutableList() }
-        ) {
+        Button(onClick = {
+            greetingState.add("1")
+        }) {
             Text("Add new Name")
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
