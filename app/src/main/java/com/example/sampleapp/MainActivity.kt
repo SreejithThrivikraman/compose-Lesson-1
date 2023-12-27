556c735e27e25920f0c94c64695847c482728c8d
sreejiththrivikraman <mail.sreejith.23@gmail.com>
2023-12-27 17:02:07 -0500

    Implemented state hoisting logic

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sampleapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -19,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.w3c.dom.NameList
 
 
 val nameList = mutableStateListOf("Jacob", "Raswan", "Zaheer")
@@ -27,40 +29,50 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting()
-
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting() {
+fun MainScreen(){
     val greetingState by remember { mutableStateOf(nameList) }
-    var newName by remember { mutableStateOf("")}
+    val newNameState = remember { mutableStateOf("")}
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
-    ) {
+    ){
+        Greeting(greetingState,
+                 newNameState.value,
+                 { greetingState.add(newNameState.value)
+                   newNameState.value = ""
+                 },
+                 { newName -> newNameState.value = newName })
+    }
+}
+
+
+@Composable
+fun Greeting(greetingState : List<String>,
+             newName :String,
+             buttonClick : () -> Unit,
+             textFieldUpdate : (newName: String) -> Unit) {
 
         greetingState.forEach { item ->
             Text(text = item)
         }
 
-        TextField(value = newName, onValueChange = {newName = it}, label = { Text("Enter new Name")})
+        TextField(value = newName, onValueChange = textFieldUpdate , label = { Text("Enter new Name")})
 
-        Button(onClick = {
-            greetingState.add(newName)
-            newName = ""
-        }) {
+        Button(onClick =  buttonClick) {
             Text("Add new Name")
         }
-    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
 fun ViewPreview() {
-    Greeting()
+    MainScreen()
 }
